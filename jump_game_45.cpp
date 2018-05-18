37699437b51b7a64693c53a910f163f3236c15e5
root <root@barlder13.(none)>
2018-05-18 21:13:27 +0800

    jump game

---
+++ jump_game_45.cpp
@@ -0,0 +1,63 @@
+/*************************************************************************
+	> File Name: jump_game_45.cpp
+	> Author: clhongooo
+	> Mail: clhongooo@163.com 
+	> Created Time: Fri 18 May 2018 06:23:08 PM CST
+ ************************************************************************/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int jump(vector<int>& nums)
+{
+	if(nums.size() <= 1)
+	{
+		return 0;
+	}
+	if(nums.size() <= 2)
+	{
+		return 1;
+	}
+
+	vector<int> step_vec;
+	for(int i = 1; i <= nums[0]; i++)
+	{
+		vector<int> vec(nums.begin() + i, nums.end());
+		if(nums[0] >= vec.size())
+		{
+			break;
+		}
+		else
+		{
+			step_vec.push_back(jump(vec));
+		}
+	}
+
+	if(step_vec.size() >= 1)
+	{
+		int min_value = step_vec[0];
+		for(int i = 0; i < step_vec.size(); i++)
+		{
+			if(min_value > step_vec[i])
+			{
+				min_value = step_vec[i];
+			}
+		}
+		return min_value + 1;
+	}
+
+	return 1;
+}
+
+int main()
+{
+	vector<int> test_vec;
+//	test_vec.push_back(1);
+	test_vec.push_back(2);
+	test_vec.push_back(1);
+	test_vec.push_back(1);
+	test_vec.push_back(1);
+	cout << jump(test_vec);
+	return 0;
+}
