352d12c9c20fc21c79e514ca674fd646e05d5488
clhongooo <clhongooo@clhongooodeMBP.lan>
2018-06-13 00:47:55 +0800

    optimization

--- substring_with_concatenation_of_all_words_30.cpp
+++ substring_with_concatenation_of_all_words_30.cpp
@@ -8,8 +8,10 @@
 #include<iostream>
 #include<string>
 #include<vector>
+#include<unordered_map>
 using namespace std;
 
+#if 0
 vector<int> findSubstring(string s, vector<string>& words)
 {
 	vector<int> result_vec;
@@ -39,6 +41,68 @@ vector<int> findSubstring(string s, vector<string>& words)
 		}
 		sort(vec.begin(), vec.end());
 		if(words == vec)
+		{
+			result_vec.push_back(i);
+		}
+	}
+	
+	return result_vec;
+}
+
+#endif
+
+vector<int> findSubstring(string s, vector<string>& words)
+{
+	vector<int> result_vec;
+
+	if(words.size() == 0)
+	{
+		return result_vec;
+	}
+
+	int words_len = 0;
+	for(auto item : words)
+	{
+		words_len += item.size();
+	}
+	int word_len = words_len / words.size();
+
+	unordered_map<string, int> str_map;
+	for(auto item : words)
+	{
+		if(str_map.count(item))
+		{
+			str_map[item] += 1;
+		}
+		else
+		{
+			str_map[item] = 1;
+		}
+	}
+
+	int str_len = s.size();
+	for(int i = 0; i < str_len-words_len; i++)
+	{
+		vector<string> vec;
+		string sub_str = s.substr(i, words_len);
+		unordered_map<string, int> str_map1 = str_map;
+		for(int j = 0; j < words_len; j += word_len)
+		{
+			unordered_map<string, int>::iterator iter = str_map1.find(sub_str.substr(j, word_len));
+			if(iter != str_map1.end())
+			{
+				iter->second -= 1;
+				if(iter->second == 0)
+				{
+					str_map1.erase(iter);
+				}
+			}
+			else
+			{
+				break;
+			}
+		}
+		if(str_map1.size() == 0)
 		{
 			result_vec.push_back(i);
 		}
