7749ca2442b206afe51c9c62b914cfa2fc907bff
clhongooo <clhongooo@163.com>
2018-06-20 15:00:29 +0800

    forget save the source code

--- recover_binary_search_tree_99.cpp
+++ recover_binary_search_tree_99.cpp
@@ -11,13 +11,54 @@ struct TreeNode
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
-    void foreTraverse(TreeNode* root, vector<TreeNode*>& vec)
+void foreTraverse(TreeNode* root, vector<TreeNode*>& vec)
+{
+    if(root == NULL)
     {
-        if(root == NULL)
-        {
-            return;
-        }
-        foreTraverse(root->left, vec);
-        vec.push_back(root);
-        foreTraverse(root->right, vec);
+        return;
     }
+    foreTraverse(root->left, vec);
+    vec.push_back(root);
+    foreTraverse(root->right, vec);
+}
+
+void recoverTree(TreeNode* root) 
+{
+    vector<TreeNode*> result_vec;
+    foreTraverse(root, result_vec);
+    TreeNode *small = NULL, *big = NULL;
+    int vec_size = result_vec.size();
+    for(int i = 0; i < vec_size - 1; i++)
+    {
+        if(result_vec[i]->val > result_vec[i+1]->val)
+        {
+            big = result_vec[i];
+            break;
+        }
+    }
+    for(int i = vec_size - 1; i > 0; i--)
+    {
+        if(result_vec[i]->val < result_vec[i-1]->val)
+        {
+            small = result_vec[i];
+            break;
+        }
+    }
+    if(small && big)
+    {
+        int tmp = small->val;
+        small->val = big->val;
+        big->val = tmp;
+    }
+}
+
+int main()
+{
+    TreeNode* root = new TreeNode(1);
+    root->left = new TreeNode(3);
+    root->right = NULL;
+    root->left->left = NULL;
+    root->left->right = new TreeNode(2);
+    recoverTree(root);
+    return 0;
+}
