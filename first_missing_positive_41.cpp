c28ba89b82a742d2fa6701763e565a9ab8267b98
clhongooo <clhongooo@163.com>
2018-09-07 15:28:51 +0800

    first missing positive

--- first_missing_positive_41.cpp
+++ first_missing_positive_41.cpp
@@ -2,7 +2,35 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * The commented solution has a O(nlog(n)) time complexity, then the next's time complexity is O(n).
+ * In fact, for the worst time complexity with a O(C) space complexity, the best time complexity is
+ * just O(nlog(n)).So we can not sort all vector in this problem with a O(n) time complexity.Then 
+ * there is a important point that is 'v[i] == i' in the positive integer sort.
+ */ 
+
 int firstMissingPositive(vector<int>& nums) { 
+#if 0
+    sort(nums.begin(), nums.end());
+    int positive_count = 1;
+    int res = 0;
+    for(auto i : nums){
+        if(i < 1){
+            continue;
+        }
+        
+        if(i > positive_count){
+            res = positive_count;   
+            break;
+        }else if(i == positive_count){
+            positive_count++;
+        }
+    }
+    if(res == 0){
+        res = positive_count;
+    }
+    return res;
+#endif
     int i = 0;
     int sz = nums.size();
     while(i < sz){
